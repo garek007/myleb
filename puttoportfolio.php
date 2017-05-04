43adb137fd4ff2501e01425e43bcdd16c51d21c9
Stan Alachniewicz <stan_alach@hotmail.com>
2017-05-03 18:17:35 -0700

    adding image uploader

---
+++ puttoportfolio.php
@@ -0,0 +1,45 @@
+<?php
+require('vendor/exacttarget/exacttarget.php');
+$wsdl = 'https://webservice.s4.exacttarget.com/etframework.wsdl';
+$etusername = '';//Enter the username you use to login to Exact Target
+$etpassword = '';//Enter the password you use to login to Exact Target
+//method
+
+
+
+try {
+ 
+    $client = new ExactTargetSoapClient($wsdl, array('trace'=>1));
+		$client->username = $etusername;
+    $client->password = $etpassword;
+ 
+    $port = new ExactTarget_Portfolio();
+    $port->DisplayName = $DisplayName;
+    //$port->CustomerKey = 'API Uploaded Test v10';
+    $rs = new ExactTarget_ResourceSpecification();  
+    $rs->URN = $ET_file;
+    $port->Source = $rs;
+    $port->FileName = $DisplayName;
+		//$port->FileURL="myfileurl";
+    $object = new SoapVar($port, SOAP_ENC_OBJECT, 'Portfolio', "http://exacttarget.com/wsdl/partnerAPI");
+ 
+    $request = new ExactTarget_CreateRequest();
+    $request->Options = NULL;
+    $request->Objects = array($object);
+ 
+    $results = $client->Create($request);
+		//echo '<pre>';
+    //print_r($results);
+ 		//echo '</pre>';
+		$myvar = 	array(
+			'url'=> 'http://image.updates.sandiego.org/lib/fe9e15707566017871/m/5/'.$filename
+		);
+		echo json_encode($myvar);
+		
+} catch (SoapFault $e) {
+	echo '<pre>';
+    print_r($e);
+		echo '</pre>';
+}
+
+?>
